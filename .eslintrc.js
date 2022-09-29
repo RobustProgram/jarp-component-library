938cd9b189a93c4316e035c0a7998e133ead1d90
Robustprogram <robustprogram@users.noreply.github.com>
2022-09-30 00:33:30 +1000

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'react',
+    'testing-library',
+    'prettier',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 'error',
+  },
+  overrides: [
+    {
+      files: '*.slice.ts',
+      rules: {
+        'no-param-reassign': 0,
+      },
+    },
+    {
+      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching testing files!
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        indent: 'off',
+        '@typescript-eslint/indent': 'off',
+      },
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['serviceWorker.js', '.eslintrc.js', '*.scss', '*.svg'],
+};
